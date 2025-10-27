3af4d0a4736d4c584f0733f25fd07b6cdc38c30b
nathangartlan2 <nathangartlan@gmail.com>
2025-10-27 13:38:12 -0500

    added GET park

--- api/Repositories/PostGresParksRepository.cs
+++ api/Repositories/PostGresParksRepository.cs
@@ -19,9 +19,50 @@ public class PostGresParksRepository : IParksRepository
 
     }
 
-    public Task<IResult> GetParkAsync(string id)
+    public async Task<IResult> GetParkAsync(string id)
     {
-        throw new NotImplementedException();
+        Park park;
+        await using var conn = new NpgsqlConnection(_connectionString);
+        await conn.OpenAsync();
+
+        var sql = @"SELECT
+		p.id,
+		p.name, 
+		p.state_code, 
+		p.latitude, 
+		p.longitude,
+		COALESCE(
+						json_agg(
+							json_build_object('Name', a.name, 'description', a.description)
+						) FILTER (WHERE a.id IS NOT NULL),
+						'[]'
+					) as activities
+		FROM parks p
+		LEFT JOIN activities a ON p.id = a.park_id
+		WHERE p.id = 1
+		GROUP BY p.id, p.name, p.state_code, p.latitude, p.longitude;";
+
+        await using var cmd = new NpgsqlCommand(sql, conn);
+        await using var reader = await cmd.ExecuteReaderAsync();
+
+        while (await reader.ReadAsync())
+        {
+            park = new Park
+            {
+                Id = reader.GetInt32(0).ToString(),
+                Name = reader.GetString(1),
+                StateCode = reader.GetString(2),
+                Latitude = reader.GetFloat(3),
+                Longitude = reader.GetFloat(4),
+                Activities = System.Text.Json.JsonSerializer.Deserialize<Activity[]>(reader.GetString(5)) ??
+                 Array.Empty<Activity>()
+            };
+
+            return Results.Ok(park);
+        }
+
+
+        return Results.NotFound();
     }
 
     public async Task<IResult> GetParksAsync()
