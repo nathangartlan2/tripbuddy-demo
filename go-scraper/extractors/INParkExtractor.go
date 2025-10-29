1bd03049180e2da18abf47bcd89333234ff7403c
nathangartlan2 <nathangartlan@gmail.com>
2025-10-29 10:26:04 -0500

    can scrape indiana

--- go-scraper/extractors/INParkExtractor.go
+++ go-scraper/extractors/INParkExtractor.go
@@ -1,4 +1,53 @@
 package extractors
 
+import (
+	"fmt"
+	"scraper/models"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
 type INParkExtractor struct {
+}
+
+func (s *INParkExtractor) ExtractParkData(e *colly.HTMLElement) *models.Park{
+	fmt.Println("[Level 2] Extracting park details from:", e.Request.URL)
+
+	// Extract park information
+	parkName := e.ChildText("h1")
+	latitude := 41
+	longitude := -86
+
+	fmt.Printf("[Level 2] Park Name: %s\n", parkName)
+
+	activities := []models.ParkActivity{}
+
+
+	e.ForEach("div#Activities li", func(_ int, el *colly.HTMLElement) {
+
+          activityName := strings.TrimSpace(el.Text)
+          // href := el.Attr("href")
+
+          if activityName != "" {
+              activity := models.ParkActivity{
+                  Name:        activityName,
+                  Description: "",
+              }
+              activities = append(activities, activity)
+          }
+      })
+
+	// Only return park if we have valid data
+	if parkName != ""  {
+		return &models.Park{
+			Name:       parkName,
+			StateCode:  "IL", // Illinois - could be extracted from page if needed
+			Latitude:   float32(latitude),
+			Longitude:  float32(longitude),
+			Activities: activities,
+		}
+	}
+
+	return nil
 }
--- go-scraper/scraper.go
+++ go-scraper/scraper.go
@@ -8,17 +8,18 @@ import (
 
 
 func main() {
-	extractor := &extractors.ILParkExtractor{}
+	extractor := &extractors.INParkExtractor{}
 	il := scrapers.NewILParkScraper(5, extractor)
 
-	output, _, _ := il.ScrapePark("https://dnr.illinois.gov/parks/park.starvedrock.html")
+	output, _, _ := il.ScrapePark("https://www.in.gov/dnr/state-parks/parks-lakes/turkey-run-state-park")
 
-	for i := 0; i < 10; i++{
-		il.ScrapePark("https://dnr.illinois.gov/parks/park.starvedrock.html")
-		if i % 50 == 0{
-			fmt.Printf("Scraped %d", i)
-		}
-	}
+	// for i := 0; i < 10; i++{
+	// 	il.ScrapePark("https://dnr.illinois.gov/parks/park.starvedrock.html")
+	// 	if i % 50 == 0{
+	// 		fmt.Printf("Scraped %d", i)
+	// 	}
+	// }
 
 	fmt.Println(output)
+	
 }
